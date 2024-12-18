d49304d66365af746c6a3e06b1defd3d9e28ad56
Oleg <oleg0kofficial@gmail.com>
2024-12-18 16:59:17 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const screamer = document.getElementById("screamer");
-
+const audio = document.getElementById("scream");
 
 const Btn = document.getElementById("myBtn").onclick = function(){
     screamer.style.display="block"; 
-    
+    audio.style.display="block"
 };
+ 
